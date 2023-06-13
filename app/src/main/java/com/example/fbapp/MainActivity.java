c11a573185229461cda7eb7f41506e6f8d7c1b74
unknown <arivelontsihoarana.avozara@gmail.com>
2023-06-13 10:34:30 +0300

    design ok

--- app/src/main/java/com/example/fbapp/MainActivity.java
+++ app/src/main/java/com/example/fbapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.fbapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_setting:
+                //settings
+
+                Intent settings = new Intent(this, AccountActivity.class);
+                startActivity(settings);
+                break;
+
+            default:
+                // select an option
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
